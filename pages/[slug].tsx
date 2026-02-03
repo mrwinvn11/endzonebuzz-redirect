9e6f7b06efbb9a7ad58b9ea1d15d12ba1a144f93
mrwinvn11 <152870786+mrwinvn11@users.noreply.github.com>
2026-02-03 09:30:46 +0700

    Update [slug].tsx

--- pages/[slug].tsx
+++ pages/[slug].tsx
@@ -14,6 +14,7 @@ export async function getServerSideProps(context: any) {
         destination: domain + "/blog/" + context.params.slug,
       },
     };
+    
   }
   const data = await fetchMeta(domain + "/blog/" + context.params.slug);
   return {
