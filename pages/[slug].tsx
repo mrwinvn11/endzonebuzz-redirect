0edc48eb6df7c87f719eb6ba6390eaeed5bd863a
mrwinvn11 <152870786+mrwinvn11@users.noreply.github.com>
2026-06-26 15:28:54 +0700

    Update [slug].tsx

--- pages/[slug].tsx
+++ pages/[slug].tsx
@@ -13,7 +13,6 @@ export async function getServerSideProps(context: any) {
         permanent: false,
         destination: domain + "" + context.params.slug,
       },
-
     };
   }
   const data = await fetchMeta(domain + "" + context.params.slug);
